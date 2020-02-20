afb5c4e1eb901b543fe3510651d19c2f41b214ba
crisbennie19 <akowechristian@gmail.com>
2020-02-20 10:38:02 +0100

    lastest

--- src/app/views/users/user-report/user-report.component.ts
+++ src/app/views/users/user-report/user-report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { DataService } from 'src/app/data.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-report',
@@ -7,16 +9,33 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
   styleUrls: ['./user-report.component.scss']
 })
 export class UserReportComponent implements OnInit {
-
+listData:[];
   constructor(
     @Inject(MAT_DIALOG_DATA) public selectedRow:any,
-    private dialogRef: MatDialogRef<UserReportComponent>
+    private dialogRef: MatDialogRef<UserReportComponent>,
+    private data:DataService
   ) { }
 
   ngOnInit() {
+    this.getUsersList();
   }
   closeDialog(){
     this.dialogRef.close()
     event.preventDefault()
+  }
+  getUsersList(){
+    
+    this.data.usersService.getUsers(0,1000)
+    .pipe(
+      map( res => res['data'])
+    )
+    .subscribe( (res:any) => {
+     this.listData = res
+      
+   console.log(this.listData); 
+
+    
+  });
+
   }
 }
